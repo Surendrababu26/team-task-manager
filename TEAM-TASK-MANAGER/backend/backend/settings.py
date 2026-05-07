16fcfa757917fcf7003cadf8c1dc5e5dad5501eb
Surendra26 <yanamadalasurendra@gmai.com>
2026-05-07 18:13:01 +0530

    Configure API Base URL using environment variables

--- TEAM-TASK-MANAGER/backend/backend/settings.py
+++ TEAM-TASK-MANAGER/backend/backend/settings.py
@@ -6,6 +6,8 @@ import dj_database_url
 
 load_dotenv()
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,7 +81,6 @@ DATABASES = {
         conn_max_age=600
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
